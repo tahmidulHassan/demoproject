e2f2d38de5c2c2640aaafbf7c216622e8c19b453
Your Name <you@example.com>
2021-09-19 20:15:53 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    layers: ["base","components", "utilities"],
+    content: ['./**/*.html'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
